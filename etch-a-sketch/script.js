c514e871e24b4dfbce4cebfdcd6caf57d63bb5f7
RayCodes50 <przemox2@gmail.com>
2026-01-27 16:04:09 +0000

    Create tiles dynamically
    I created logic to change tiles width and height dynamically changed
    event listener from change to input. That saved some resources.
    Event listener for hover was not applied correctly as Node was empty
    on page load so was moved to a function.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,19 +1,48 @@
 const pad = document.querySelector(".sketch");
 const inputGrid = document.querySelector(".gridSize");
+const box = document.querySelector(".box");
 
-inputGrid.addEventListener("input", (e) => {
-  const input = e.target.value;
-  console.log(input);
+// Default grid at page load
+let num = 5;
+
+inputGrid.addEventListener("change", (e) => {
+  // change users input to number
+  const value = Number(e.target.value);
+  //throws an Error when we wan to create grid larger than 100 x100
+  if (value > 100) {
+    return console.log(`Grid cannot be larger than 100`);
+  }
+  // checks for "" since number "" is 0
+  if (!Number.isNaN(value)) {
+    num = value;
+  }
+  console.log(num);
+  pad.style.setProperty("--tiles", num);
+
+  createGrid(num);
 });
 
-function createGrid() {
+function createGrid(num) {
+  // modern way of creating DOM elements that batches it and ships all
+  // at once
   const fragment = document.createDocumentFragment();
-  let size = 5;
+  let size = num;
   for (let i = 0; i < size * size; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
     fragment.appendChild(box);
   }
-  pad.appendChild(fragment);
+  // modern version of replacing DOM elements need it since there is a
+  // starting grid
+
+  pad.replaceChildren(fragment);
+  const tiles = document.querySelectorAll(".box");
+
+  tiles.forEach((tile) => {
+    console.log(`initiated`);
+    tile.addEventListener("mouseenter", (e) => {
+      console.log(`You just hovered over me`);
+    });
+  });
 }
-createGrid();
+createGrid(num);
