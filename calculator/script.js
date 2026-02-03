f8673f491625a3cca6538bd12c5dedb99ae2df77
RayCodes50 <przemox2@gmail.com>
2026-02-03 14:41:32 +0000

    Adds additional digit numbers
    Previously user could only operate on single digit numbers now
    the user can work with unlimited numner of digits. Still no 2.9 though
    It fixes the display issue that it would not clear the display and
    show 888 when pressed 8 twice

--- calculator/script.js
+++ calculator/script.js
@@ -14,14 +14,37 @@ equal.addEventListener("click", function () {
 btnsOperators.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     operator = e.target.innerText;
-    displayCurrent.innerText += ` x`;
+    displayCurrent.innerText += ` ${operator}`;
     console.log(operator);
   });
 });
 
 btnsNumbers.forEach((number) => {
   number.addEventListener("click", (e) => {
-    let number = Number(e.target.innerText);
+    let number = e.target.innerText;
+    // checks if operator pressed if not adds additional number to first
+    // like 1 click 2 instead of operator creates 12
+    if (operator == "" && firstNumber != 0) {
+      //changes to string to concatinate most likely there is a better solution
+      let temp = firstNumber.toString();
+      // same here as up
+      let additional = number.toString();
+      // makes 12 from 1 and 2 and then changes to number just in case
+      firstNumber = Number(temp + additional);
+      // updates the display accordingly, e.g. displays first number correctly
+      displayCurrent.innerText = `${firstNumber}`;
+      return;
+    } else if (operator != "" && secondNumber != 0) {
+      let temp = secondNumber.toString();
+      let additional = number.toString();
+      secondNumber = Number(temp + additional);
+      //this line below resets the display after trying to create two digit
+      // second number without it when pressing 8 and then 8 again it would
+      // give 888 cos display would still hold 8 from before
+      displayCurrent.innerText = `${firstNumber + " " + operator}`;
+      displayCurrent.innerText += ` ${secondNumber}`;
+      return;
+    }
     if (firstNumber == 0) {
       firstNumber = number;
       displayCurrent.innerText = `${number}`;
