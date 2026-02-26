8039dd54774459c282a28d7e4d0bd2cc4e4a711e
RayCodes50 <przemox2@gmail.com>
2026-02-26 11:33:13 +0000

    Forgot to commit early and often..to many changes

--- myLibrary/script.js
+++ myLibrary/script.js
@@ -1,15 +1,56 @@
+const container = document.querySelector(".container");
+const addForm = document.querySelector(".add");
+const form = document.querySelector(".form");
+const formTitle = document.querySelector(".title");
+const formAuthor = document.querySelector(".author");
+const formPages = document.querySelector(".pages");
+const formRead = document.querySelector(".read");
+const formBtn = document.querySelector(".formBtn");
 const myLibrary = [];
 
+//Hides form in the DOM and toggles it being displayed
+addForm.addEventListener("click", () => {
+  form.classList.toggle("visibility");
+});
+
+//prevents form from refreshing the page
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+//adds book to the arr by user
+formBtn.addEventListener("click", function () {
+  addBookToLibrary(
+    formTitle.value,
+    formAuthor.value,
+    formPages.value,
+    formRead.value,
+  );
+  displayBooks();
+
+  console.log(myLibrary);
+});
+
+// book constructor
 function Book(title, author, pages, read) {
-  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = crypto.randomUUID();
 }
-
+// adds book to the library (arr)
 function addBookToLibrary(title, author, pages, read) {
   const book1 = new Book(title, author, pages, read);
+  myLibrary.push(book1);
 }
 
-console.log(addBookToLibrary(""));
+// display book algo
+function displayBooks(book) {
+  myLibrary.forEach((element) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerText = `${element.title} by ${element.author}, ${element.pages} and ${element.read}`;
+    container.appendChild(div);
+  });
+}
