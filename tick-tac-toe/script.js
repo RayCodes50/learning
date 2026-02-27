d1dc62a6349dfa227d47ce3eeeba3db084f08629
RayCodes50 <przemox2@gmail.com>
2026-02-27 14:42:37 +0000

    Player constructor created

--- tick-tac-toe/script.js
+++ tick-tac-toe/script.js
@@ -7,5 +7,24 @@ const BoardFactory = (function () {
   }
   return { board };
 })();
+const board = BoardFactory.board();
+board.gameBoard[1] = "X";
+board.gameBoard[5] = "O";
+console.log(board.gameBoard);
 
-console.log(BoardFactory.board());
+// Player constructor
+function Player(name, marker) {
+  if (!new.target) {
+    throw Error("You must use the new operator to call the constructor");
+  }
+  this.name = name;
+  this.marker = marker;
+}
+
+//create a new player1
+const player1 = new Player("Steve", "X");
+console.log(player1.marker);
+
+// the part of game logic?
+board.gameBoard[2] = player1.marker;
+console.log(board.gameBoard);
