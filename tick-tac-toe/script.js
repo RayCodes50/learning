e486ee25309b7e039b82a259023140797396fd26
RayCodes50 <przemox2@gmail.com>
2026-02-28 09:38:54 +0000

    Create GameController factory

--- tick-tac-toe/script.js
+++ tick-tac-toe/script.js
@@ -1,5 +1,4 @@
 // FActory wrapped in IIFE (immediately invoked function expression)
-//
 const BoardFactory = (function () {
   function board() {
     const gameBoard = Array(9).fill(null);
@@ -9,22 +8,32 @@ const BoardFactory = (function () {
 })();
 const board = BoardFactory.board();
 board.gameBoard[1] = "X";
-board.gameBoard[5] = "O";
 console.log(board.gameBoard);
 
 // Player constructor
-function Player(name, marker) {
-  if (!new.target) {
-    throw Error("You must use the new operator to call the constructor");
+const CreatePlayer = (function () {
+  function createPlayer(name, marker) {
+    return { name, marker };
   }
-  this.name = name;
-  this.marker = marker;
-}
+  return { createPlayer };
+})();
 
 //create a new player1
-const player1 = new Player("Steve", "X");
+const player1 = CreatePlayer.createPlayer("Steve", "X");
 console.log(player1.marker);
+player1.crossField(3);
 
+const GameController = function () {
+  //handle turns
+  //switch players
+  // check winner
+};
 // the part of game logic?
 board.gameBoard[2] = player1.marker;
+// board.gameBoard[4] = player2.marker;
 console.log(board.gameBoard);
+
+// crossing field logic
+// function crossField(position) {
+//   return (board.gameBoard[position] = marker);
+// }
